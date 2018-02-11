498907a0233012757e10b60231c627ac5531ad86
renit24 <ren24ai31@gmail.com>
2018-02-11 17:25:12 +0900

    All-Information-Tool

--- All-Information-Tool/All-Information-Tool/Form1.Designer.cs
+++ All-Information-Tool/All-Information-Tool/Form1.Designer.cs
@@ -28,12 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "天気";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "東京";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::All_Information_Tool.Properties.Resources.sozai_cman_jp_20180211170203;
+            this.pictureBox1.Location = new System.Drawing.Point(9, 34);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(80, 60);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(476, 261);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- All-Information-Tool/All-Information-Tool/Form1.cs
+++ All-Information-Tool/All-Information-Tool/Form1.cs
@@ -4,9 +4,12 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json.Linq;
 
 namespace All_Information_Tool
 {
@@ -15,6 +18,72 @@ namespace All_Information_Tool
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ////今日の天気予報の取得   
+            string baseUrl = "http://weather.livedoor.com/forecast/webservice/json/v1";
+            //東京都のID
+            string cityname = "340010";
+
+            string url = $"{baseUrl}?city={cityname}";
+            string json = new HttpClient().GetStringAsync(url).Result;
+            JObject jobj = JObject.Parse(json);
+
+            string todayweather = (string)((jobj["forecasts"][0]["telop"] as JValue).Value);//今日の天気の取得
+            label2.Text = (todayweather);
+            switch(todayweather)
+            {
+                case "晴":
+                    this.pictureBox1.Image = Properties.Resources.sozai_cman_jp_20180211170221;
+                    break;
+                case "晴れ時々曇り":
+                    this.pictureBox1.Image = Properties.Resources.sozai_cman_jp_20180211170221;
+                    break;
+                case "晴れ時々雨":
+                    this.pictureBox1.Image = Properties.Resources.sozai_cman_jp_20180211170221;
+                    break;
+                case "晴れ時々雪":
+                    this.pictureBox1.Image = Properties.Resources.sozai_cman_jp_20180211170221;
+                    break;
+                case "晴れのち曇り":
+                    this.pictureBox1.Image = Properties.Resources.sozai_cman_jp_20180211170221;
+                    break;
+                case "晴れのち雨":
+                    this.pictureBox1.Image = Properties.Resources.sozai_cman_jp_20180211170221;
+                    break;
+                case "晴れのち雪":
+                    this.pictureBox1.Image = Properties.Resources.sozai_cman_jp_20180211170221;
+                    break;
+                case "曇り":
+                    this.pictureBox1.Image = Properties.Resources.sozai_cman_jp_20180211170221;
+                    break;
+                case "曇り時々晴れ":
+                    this.pictureBox1.Image = Properties.Resources.sozai_cman_jp_20180211170221;
+                    break;
+                case "曇り時々雨":
+                    this.pictureBox1.Image = Properties.Resources.sozai_cman_jp_20180211170221;
+                    break;
+                case "曇り時々雪":
+                    this.pictureBox1.Image = Properties.Resources.sozai_cman_jp_20180211170221;
+                    break;
+                case "曇りのち晴れ":
+                    this.pictureBox1.Image = Properties.Resources.sozai_cman_jp_20180211170221;
+                    break;
+                case "曇りのち雨":
+                    this.pictureBox1.Image = Properties.Resources.sozai_cman_jp_20180211170221;
+                    break;
+                case "曇りのち雪":
+                    this.pictureBox1.Image = Properties.Resources.sozai_cman_jp_20180211170221;
+                    break;
+                case "雨":
+                    this.pictureBox1.Image = Properties.Resources.sozai_cman_jp_20180211170221;
+                    break;
+                case "雨時々晴れ":
+                    this.pictureBox1.Image = Properties.Resources.sozai_cman_jp_20180211170221;
+                    break;
+            }
         }
     }
 }
